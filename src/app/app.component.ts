11f8364245eb7259d6545c7c642cc8f9a3b4ca0b
shanegibney <shanegibney@gmail.com>
2017-07-17 00:30:18 +0100

    updates to UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -231,29 +231,3 @@ check(){
     this.negativeanswers = true;
   }
 }
-
-@Directive({
-    selector: '[autofocus]'
-})
-export class AutofocusDirective
-{
-    private _autofocus;
-    constructor(private el: ElementRef, private renderer: Renderer)
-    {
-    }
-
-    ngOnInit()
-    {
-    }
-
-    ngAfterViewInit()
-    {
-        if (this._autofocus || typeof this._autofocus === "undefined")
-            this.renderer.invokeElementMethod(this.el.nativeElement, 'focus', []);
-    }
-
-    @Input() set autofocus(condition: boolean)
-    {
-        this._autofocus = condition != false;
-    }
-}
